313cf35ccdf9397fca997195a4e01ae614c64ec5
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-04-01 22:37:48 +0530

    api object definition

---
+++ apiobject.sh
@@ -0,0 +1,24 @@
+
+#Get the information about the current context 
+kubectl config get-contexts
+
+#Change the context
+kubectl config use-context kubernetes-admin@kubernetes
+
+#get cluster info
+kubectl cluster-info
+
+
+#Get a list of api-resources at the cluster
+kubectl api-resources 
+
+#Explain the pods
+kubectl explain pods 
+
+#Creating a pod
+kubectl apply -f pod.yaml
+
+#Detail of the pod specifiction
+kubectl explain pod.spec
+kubectl explain pod.spec.containers
+
