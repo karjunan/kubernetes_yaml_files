34ad6679cda29dd897fe75a379c3aefe39d62814
krishna <Krishnakumar.Arjunan@pickles.com.au>
2020-04-01 23:34:59 +0530

    deploymevt

--- apiobject.sh
+++ apiobject.sh
@@ -25,3 +25,27 @@ kubectl explain pod.spec.containers
 
 #Delete a pod
 kubectl delete pod hello-world
+
+#Explain api resources
+kubectl api-resources
+
+#Explain deployment object
+kubectl explain deployment
+
+#Explain deployment object higher version
+kubectl explain deployment --api-version apps/v1beta2
+
+
+#Explain deployment object which is a stable version
+kubectl explain deployment --api-version apps/v1
+
+
+#Explain Api versions
+kubectl api-versions
+
+#Instruction from kubectl to deploy a yaml file
+kubectl apply -f deployment.yaml
+
+kubectl get deployment hello-world -o yaml
+
+kubectl delete deployment hello-world
